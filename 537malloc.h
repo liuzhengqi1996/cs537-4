de48b85eb2b17456345259f6ae9c54a667034277
Zhengqi Liu <37845374+liuzhengqi1996@users.noreply.github.com>
2018-12-03 00:57:05 -0600

    Update 537malloc.h

--- 537malloc.h
+++ 537malloc.h
@@ -8,11 +8,14 @@
 // CS Login:         zhengqi, tian
 // NetID:            mliu292, tzheng24
 /////////////////////////////////////////////////////////////////////////////////
-//
+//maolloc537 is a safe malloc function
 void * malloc537(size_t size);
 
+//free537 is a safe free function
 void free537(void *ptr);
 
+//similar to realloc, but checking on the pointer
 void *realloc537(void *ptr, size_t size);
 
+//check pointer with address and the range
 void memcheck537(void *ptr,size_t size);
